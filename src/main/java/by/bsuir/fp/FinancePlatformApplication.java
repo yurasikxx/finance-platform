592f72d0e98cb442b2eb33bd1abfced44cc62445
Yury Tkachyk <yurasikxx@gmail.com>
2026-03-09 12:48:21 +0300

    Initial commit: project generated from Spring Initializr

---
+++ src/main/java/by/bsuir/fp/FinancePlatformApplication.java
@@ -0,0 +1,13 @@
+package by.bsuir.fp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancePlatformApplication.class, args);
+	}
+
+}
